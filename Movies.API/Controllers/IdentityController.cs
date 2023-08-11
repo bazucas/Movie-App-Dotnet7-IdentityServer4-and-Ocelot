16f2233d1d985c48ca85d4d65bb9a5384b195c3a
Luís Inácio <lrs.inacio@gmail.com>
2023-08-11 16:38:50 +0100

    IdentityController filter change from sql to linq method chain

--- Movies.API/Controllers/IdentityController.cs
+++ Movies.API/Controllers/IdentityController.cs
@@ -11,6 +11,6 @@ public class IdentityController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        return new JsonResult(User.Claims.Select(c => new { c.Type, c.Value }));
     }
 }
